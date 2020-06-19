3105afd706672f059ad34ba1213bd77519c8ecea
shuxingongsir <42061175+shuxingongsir@users.noreply.github.com>
2020-06-19 23:35:51 +0800

    1.主要包括4个文件 （1）HelloController （2）springmvc.xml （3）web.xml （4）index.jsp

--- src/main/java/com/gs/controller/HelloController.java
+++ src/main/java/com/gs/controller/HelloController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
+    /**
+     * 测试
+     * @param name 用户名
+     * @param password 密码
+     * @return 视图名
+     */
     @RequestMapping("/test")
     public String test(String name, String password){
-        return "test";
+        return "hello";
     }
 }
